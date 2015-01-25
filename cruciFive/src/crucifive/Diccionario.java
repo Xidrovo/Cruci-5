2f1b6880c30f3678debf7494c7179ee0bf0789c5
Xidrovo <xidrovo@hotmail.com>
2015-01-25 16:06:52 -0400

    agregados métodos sin construcciónes
    ya me dió pereza poner los otros xD... agrego algunos métodos pero no
    tienen cuerpo.

--- cruciFive/src/crucifive/Diccionario.java
+++ cruciFive/src/crucifive/Diccionario.java
@@ -5,10 +5,37 @@
  */
 package crucifive;
 
+import java.util.LinkedList;
+
 /**
  *
  * @author fernando
  */
 public class Diccionario {
+    //Aquí un pequeño cambio, en el escrito habíamos puesto un "Array" de clase "Palabra"
+    //Ahora hay una lista vinculada que guarda palabras.
     
+    private LinkedList<Palabra> Words;
+    private String Name;
+    
+    public void AddWord(String Word)
+    {
+        //This Method will add a word on the Dictionary.
+    }
+    
+    public void ViewDictionariesList()
+    {
+        //This Method will show to the user all the Dictionaries.
+    }
+    
+    public void ViewDictionary(String DictionaryName)
+    {
+        //This Method will show all the word of an Specific Dictionary.
+    }
+    
+    public String GetWord()
+    {
+        //This Method will return a word of the Dictionary's words.
+        return "Hello Word";
+    }
 }
--- cruciFive/src/crucifive/Palabra.java
+++ cruciFive/src/crucifive/Palabra.java
@@ -11,4 +11,36 @@ package crucifive;
  */
 public class Palabra {
     
+    private String Word = new String();
+    private String Pista = new String();
+    private int WordLong;
+    
+    public Palabra()
+    {
+        Word = "";
+        Pista = "";
+        WordLong = 0;
+    }
+
+    public Palabra(int WordLong, String Word, String Pista) {
+        this.WordLong = WordLong;
+        this.Pista = Pista;
+        this.Word = Word;
+    }
+    
+    public int GetLong()
+    {
+        return WordLong;
+    }
+    
+    public void SetWord(String Word)
+    {
+        //This Method will set a word.
+    }
+     
+    public void SetPista(String Pista)
+    {
+        //this Method will set a Pista and attach it to the word.
+    }
+   
 }
