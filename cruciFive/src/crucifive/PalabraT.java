87c4da08e42e7b73c28f580578bf9f720050b44f
Xidrovo <xidrovo@hotmail.com>
2015-02-01 16:43:34 -0400

    Métodos de PalabraT agregados

--- cruciFive/src/crucifive/PalabraT.java
+++ cruciFive/src/crucifive/PalabraT.java
@@ -11,8 +11,42 @@ package crucifive;
  */
 public class PalabraT extends Palabra{
     
-    boolean orientacion;
-    int posicioni[] = new int[2];    //aqui ira la posicion en el tablero de la palabra
-    int posicionf[] = new int[2];
+    private boolean orientacion;
+    private int posicioni[] = new int[2];    //aqui ira la posicion en el tablero de la palabra
+    private int posicionf[] = new int[2];
+
+    public PalabraT() {
+        orientacion = false;
+    }
+
+    public PalabraT(String Word, String Pista, boolean orientacion) {
+        super(Word, Pista);
+        this.orientacion = orientacion;
+    }
+
+    public boolean isOrientacion() {
+        return orientacion;
+    }
+
+    public void setOrientacion(boolean orientacion) {
+        this.orientacion = orientacion;
+    }
+
+    public int[] getPosicioni() {
+        return posicioni;
+    }
+
+    public void setPosicioni(int[] posicioni) {
+        this.posicioni = posicioni;
+    }
+
+    public int[] getPosicionf() {
+        return posicionf;
+    }
+
+    public void setPosicionf(int[] posicionf) {
+        this.posicionf = posicionf;
+    }
+    
     
 }
